0fc3ac0183fec55bfd749d73033b66c21022e39c
KirillZ <kzasorin@gmail.com>
2022-04-16 21:28:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Revive HDESK
+
+Sample app for learn Laravel framework.
+
+### Main idea
+This application is created in order to learn the basics of the Laravel framework and at the same time solve some practical problem for myself.
+
+The idea behind the app is to keep track of clients and jobs for a self-employed IT professional these days. It is also planned to implement accounting of projects and time spent. It is also a list of services, calculation of payments by clients and so on.
+
+
---
+++ resources/views/client/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+
+            <form action="{{ route('clients.update', $clients->id) }}" method="post" class="row g-3">
+                @csrf
+                @method('patch')
+                <div class="col-md-6">
+                    <label for="name" class="form-label">Имя</label>
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Имя">
+                </div>
+                <div class="col-md-6">
+                    <label for="city" class="form-label">Город</label>
+                    <input type="text" name="city" class="form-control" id="city" placeholder="Город">
+                </div>
+                <div class="col-12">
+                    <label for="address1" class="form-label">Адрес 1</label>
+                    <input type="text" name="address1" class="form-control" id="address1" placeholder="Адрес 1">
+                </div>
+                <div class="col-12">
+                    <label for="address2" class="form-label">Адрес 2</label>
+                    <input type="text"  name="address2" class="form-control" id="address2" placeholder="Адрес 2">
+                </div>
+                <div class="col-md-4">
+                    <label for="state" class="form-label">Регион</label>
+                    <select id="state" class="form-select">
+                        <option selected>Выберите...</option>
+                        <option>Россия</option>
+                    </select>
+                </div>
+                <div class="col-md-2">
+                    <label for="postal_code" class="form-label">Почтовый индекс</label>
+                    <input type="text" name="postal_code" class="form-control" id="postal_code" placeholder="Почтовый индекс">
+                </div>
+                <div class="col-12">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="gridCheck">
+                        <label class="form-check-label" for="gridCheck">
+                            Чек ми аут
+                        </label>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-outline-dark">Обновить</button>
+                </div>
+            </form>
+
+
+
+
+        </div>
+    </div>
+@endsection
