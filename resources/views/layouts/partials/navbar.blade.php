82aa911fd1f955f235e80b276a598ab6942592c2
KirillZ <kzasorin@gmail.com>
2022-05-14 02:39:32 +0300

    Added version to Home

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -3,6 +3,7 @@
         <a class="navbar-brand" href="{{ route('home.index') }}">
             <img src="/img/logo.svg" alt="" width="100%" height="60" class="d-inline-block">
         </a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -25,5 +26,7 @@
                 </li>
             </ul>
         </div>
+      <span class="version">v {{ config('app.version') }}</span>
     </div>
 </nav>
+
