fdc073d0b95ae3c5feb4a6fb95e547fc8f4e2ff2
KirillZ <kzasorin@gmail.com>
2022-05-26 13:56:05 +0300

    #22 invoice_status 3/6 tasks
    Сделал 3 таска из 6

--- app/Constants.php
+++ app/Constants.php
@@ -11,4 +11,13 @@ if (!defined('APP_NAME')) {
     define('DB_CONNECTION', env('DB_CONNECTION', 'sqlite'));
     define('DB_FOREIGN_KEYS', env('DB_FOREIGN_KEYS', 'true'));
     define('FACTORY_COUNT', env('FACTORY_COUNT', 10));
+
+    define('INVOICE_STATUS_DRAFT', 1);
+    define('INVOICE_STATUS_SENT', 2);
+    define('INVOICE_STATUS_VIEWED', 3);
+    define('INVOICE_STATUS_APPROVED', 4);
+    define('INVOICE_STATUS_PARTIAL', 5);
+    define('INVOICE_STATUS_PAID', 6);
+    define('INVOICE_STATUS_OVERDUE', -1);
+    define('INVOICE_STATUS_UNPAID', -2);
 }
--- app/Models/InvoiceStatus.php
+++ app/Models/InvoiceStatus.php
@@ -9,12 +9,27 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class InvoiceStatus extends Model
 {
     use HasFactory;
-
+    public $timestamps = false;
     protected $table = 'invoice_statuses';
     protected $guarded = false;
 
     public function invoice_status(): BelongsTo
     {
         return $this->belongsTo('invoices');
+    }
+
+
+    public static function getIdFromAlias($status): bool|int
+    {
+        return match ($status) {
+            'draft' => INVOICE_STATUS_DRAFT,
+            'sent' => INVOICE_STATUS_SENT,
+            'viewed' => INVOICE_STATUS_VIEWED,
+            'approved' => INVOICE_STATUS_APPROVED,
+            'partial' => INVOICE_STATUS_PARTIAL,
+            'overdue' => INVOICE_STATUS_OVERDUE,
+            'unpaid' => INVOICE_STATUS_UNPAID,
+            default => false,
+        };
     }
 }
